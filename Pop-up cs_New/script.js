9c7fadb8780ee3094d75a6c0c0732159af2b31d7
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2024-01-01 14:54:05 +0530

    added esc btn to close popup bar

---
+++ Pop-up cs_New/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const showBtn = document.querySelectorAll('.show-modal');
+
+const closeModal2 = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < showBtn.length; i++) {
+  showBtn[i].addEventListener('click', openModal);
+}
+
+closeBtn.addEventListener('click', closeModal2);
+overlay.addEventListener('click', closeModal2);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    console.log('eascpe was pressed');
+    if (!model.classList.contains('hidden')) {
+      closeModal2();
+    }
+  }
+});
+
+// const closeModal = function (popclose) {
+//   popclose.addEventListener('click', function () {
+//     model.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   });
+// };
