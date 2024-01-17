a9b47cda9d670eddde26370e7fa85e66708cadca
imalKesara <imalkesara2001@gmail.com>
2024-01-17 15:13:35 +0530

    done

--- football betting application/football.js
+++ football betting application/football.js
@@ -1,6 +1,6 @@
 const game = {
-  team1: "Bayern munich",
-  team2: "Borrussia Dortmud",
+  team1: "Sri Lanka",
+  team2: "India",
   players: [
     [
       "neuser",
@@ -39,7 +39,46 @@ const game = {
     team2: 6.5,
   },
 };
+console.log("----Q1----");
 //Q1
 const [player1, player2] = game.players;
 console.log(player1);
 console.log(player2);
+
+console.log("----Q2----");
+// Q2
+const [goldKeeper, ...fieldPlayers] = player1;
+console.log(goldKeeper);
+console.log(fieldPlayers);
+
+console.log("----Q3----");
+
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+console.log("----Q4----");
+
+const newPlayers = [...player1, "Arthur", "Dutch", "Sadie", ...player2];
+console.log(newPlayers);
+
+console.log("----Q5----");
+const {
+  odds: { x, team1, team2 },
+} = game;
+console.log(x);
+console.log(team1);
+console.log(team2);
+
+console.log("----Q6----");
+
+const printGoals = function (...players) {
+  console.log(players.length);
+};
+
+// printGoals("Athur", "Morgan", "dutch", "Sadie");
+// printGoals("Kesara", "Gihan");
+printGoals(...game.scored);
+
+console.log("----Q7----");
+
+team1 < team2 && console.log("team 1 is more likely to win");
