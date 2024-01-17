9275f8379c43da640413ce4f0e379241d7e74062
imalKesara <imalkesara2001@gmail.com>
2024-01-17 14:03:13 +0530

    const game obj

---
+++ football betting application/football.js
@@ -0,0 +1,41 @@
+const game = {
+  team1: "Bayern munich",
+  team2: "Borrussia Dortmud",
+  players: [
+    [
+      "neuser",
+      "pavard",
+      "martinez",
+      "alaba",
+      "davies",
+      "kimmich",
+      "goretzka",
+      "coman",
+      "muller",
+      "gnarby",
+      "Lewandowski",
+    ],
+    [
+      "burki",
+      "schulz",
+      "hummels",
+      "akanji",
+      "hakimi",
+      "weigl",
+      "witsel",
+      "hazard",
+      "brandt",
+      "sancho",
+      "gotzu",
+    ],
+  ],
+
+  score: "4.0",
+  scored: ["Lewandowski", "gnarby", "Lewandowski", "hummels"],
+  Date: "Nov 9th 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
