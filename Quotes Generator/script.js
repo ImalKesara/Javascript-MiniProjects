4d1e2f4e95701209053adcfb6a76e6f857875fda
imalKesara <imalkesara2001@gmail.com>
2024-01-20 11:06:43 +0530

    use local quotes

--- Quotes Generator/script.js
+++ Quotes Generator/script.js
@@ -1,25 +1,27 @@
 "use strict";
 
-let apiQoutes = [];
+// let apiQoutes = [];
 
 const newQuotes = function () {
   // pick random quotes from apiQutes array
-  let quote = apiQoutes[Math.floor(Math.random() * apiQoutes.length)];
+  let quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
   console.log(quote);
+  console.log(localQuotes.length);
 };
 
 // get information from api
-async function getQuotes() {
-  const apiUrl = "https://type.fit/api/quotes";
-  try {
-    const response = await fetch(apiUrl);
-    apiQoutes = await response.json();
-    // console.log(apiQoutes[12 ]);
-    newQuotes();
-  } catch (error) {
-    // Catch error here
-    alert(error);
-  }
-}
+// async function getQuotes() {
+//   const apiUrl = "https://type.fit/api/quotes";
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQoutes = await response.json();
+//     // console.log(apiQoutes[12 ]);
+//     newQuotes();
+//   } catch (error) {
+//     // Catch error here
+//     alert(error);
+//   }
+// }
 
-getQuotes();
+// getQuotes();
+newQuotes();
