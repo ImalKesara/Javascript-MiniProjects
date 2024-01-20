3e7cdfe3b1af3539459e1d64d41df1be67a61e18
imalKesara <imalkesara2001@gmail.com>
2024-01-20 10:56:12 +0530

    random quotes pick

--- Quotes Generator/script.js
+++ Quotes Generator/script.js
@@ -2,13 +2,20 @@
 
 let apiQoutes = [];
 
+const newQuotes = function () {
+  // pick random quotes from apiQutes array
+  let quote = apiQoutes[Math.floor(Math.random() * apiQoutes.length)];
+  console.log(quote);
+};
+
 // get information from api
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
     apiQoutes = await response.json();
-    console.log(apiQoutes);
+    // console.log(apiQoutes[12 ]);
+    newQuotes();
   } catch (error) {
     // Catch error here
     alert(error);
