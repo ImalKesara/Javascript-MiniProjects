0dd3142fdcd0d5be55ad053793ffd34d25378f6c
imalKesara <imalkesara2001@gmail.com>
2024-01-20 10:41:15 +0530

    API request

--- Quotes Generator/script.js
+++ Quotes Generator/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+let apiQoutes = [];
+
+// get information from api
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQoutes = await response.json();
+    console.log(apiQoutes);
+  } catch (error) {
+    // Catch error here
+    alert(error);
+  }
+}
+
+getQuotes();
