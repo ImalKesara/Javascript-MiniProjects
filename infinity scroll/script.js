58fd3f51eb48f03ab46078f7fd4d1548c07330ff
LoneWolf <imalkesara2001@gmail.com>
2024-02-18 21:43:32 +0530

    get photos from unplash

--- infinity scroll/script.js
+++ infinity scroll/script.js
@@ -1,1 +1,17 @@
-const apiUrl = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`;
+const count = 10;
+const apiKey = "UO0d1dnw5qxi__g3AKbnjvBmnaWPiGzzNPvfgWSGTbw";
+
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count} `;
+
+//get photos from unsplash api
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console("error", error);
+  }
+}
+
+getPhotos();
