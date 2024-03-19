c6045fbb48dbc784d40b7c6768bf408d17d1e13b
LoneWolf <imalkesara2001@gmail.com>
2024-03-19 18:07:11 +0530

    div to btns

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,7 +1,33 @@
 "use strict";
-const divs = document.querySelectorAll(".btn");
-divs.forEach(function (e) {
-  e.addEventListener("click", (btn) => {
-    console.log(btn.value);
-  });
-});
+const Screen = document.querySelector(".screen");
+const clearBtn = document.querySelector(".C");
+// let numbers = document.querySelectorAll(".number");
+// let op = document.querySelectorAll(".operator");
+// let firstNumber = "";
+// let secondNumber = "";
+// let result;
+// let operator = "";
+// let add = document.querySelector(".add");
+// let mul = document.querySelector(".mul");
+// let division = document.querySelector(".division");
+// let ac = document.querySelector(".AC");
+// let substra = document.querySelector(".substras");
+// let equal = document.querySelector(".equal");
+
+// let liveScreen = "";
+
+// numbers.forEach(function (element) {
+//   element.addEventListener("click", function () {
+//     liveScreen = parseInt(this.getAttribute("value"));
+//     firstNumber = liveScreen;
+//     Screen.textContent = firstNumber;
+//   });
+// });
+
+// op.forEach(function (operator) {
+//   operator.addEventListener("click", function () {
+//     liveScreen = this.getAttribute("value");
+//     operator = liveScreen;
+//     Screen.textContent = operator;
+//   });
+// });
